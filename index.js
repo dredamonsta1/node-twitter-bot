680afc336cdabd0af869d1ceabf34b2a71df9415
andre wilkinson <andrewilkinson@andres-MacBook-Pro.local>
2019-01-06 11:08:56 -0800

    removed unneeded lines

--- index.js
+++ index.js
@@ -3,14 +3,7 @@ var config = require('./config.js');
 
 var T = new twit(config);
 
-// var T = new Twit({
-//   consumer_key:         ' ',
-//   consumer_secret:      '',
-//   access_token:         '',
-//   access_token_secret:  '',
-//   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-//   strictSSL:            true,     // optional - requires SSL certificates to be valid.
-// })
+
 
 console.log('**********bot my baby one more time lol********')
 
