add696b7f8a5025d8fdf61faf4ac74bdbd73986e
andre wilkinson <andrewilkinson@andres-MacBook-Pro.local>
2019-01-06 01:44:20 -0800

    its working just need to clean up code

---
+++ config.js
@@ -0,0 +1,14 @@
+//config.js
+/** TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
+module.exports = {
+    consumer_key: 'jshSDd5N5Q1ZPb4l511vSwtL0',  
+    consumer_secret: 'uD6Z5N81O6KFvYOMG81tnHIXAwaFLT4m7gtzeSlbIxz3Av7xNj',
+    access_token: '22637016-woGjjUDoT29ulE69mn1EkDmg9oCdDvYn7Ux9EZlHU',  
+    access_token_secret: 'FVCQcITlcVxVSkFYGE00wlngHUehzpUI2RaEzHOXNysq5'
+  }
+  
--- index.js
+++ index.js
@@ -1,10 +1,38 @@
-var Twit = require('twit')
- 
-var T = new Twit({
-  consumer_key:         'jshSDd5N5Q1ZPb4l511vSwtL0 ',
-  consumer_secret:      'uD6Z5N81O6KFvYOMG81tnHIXAwaFLT4m7gtzeSlbIxz3Av7xNj',
-  access_token:         '22637016-woGjjUDoT29ulE69mn1EkDmg9oCdDvYn7Ux9EZlHU',
-  access_token_secret:  'FVCQcITlcVxVSkFYGE00wlngHUehzpUI2RaEzHOXNysq5',
-  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-  strictSSL:            true,     // optional - requires SSL certificates to be valid.
-})
+var twit = require('twit');
+var config = require('./config.js');
+
+var T = new twit(config);
+
+// var T = new Twit({
+//   consumer_key:         ' ',
+//   consumer_secret:      '',
+//   access_token:         '',
+//   access_token_secret:  '',
+//   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+//   strictSSL:            true,     // optional - requires SSL certificates to be valid.
+// })
+
+console.log('**********bot my baby one more time lol********')
+
+// var retweet = function() {
+//     var params = {
+//         q: '#hiphop, #bestrapper, #Goat, #topfive, #cultcure, #clutcher, #Kicks, #kotd',
+//         result_type: 'recent',
+//         lang: 'en'
+//     }
+// }
+
+var tweet = {
+    status: '#hiphop, #bestrapper, #Goat, #topfive, #cultcure, #clutcher, #Kicks, #kotd'
+}
+
+T.post('statuses/update', tweet, tweeted);
+
+function tweeted(er, data, response) {
+    if(err) {
+        console.log("noooooooooooœ!");
+    } else {
+        console.log(data);
+    }
+    
+}
